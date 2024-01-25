a4066909ecabd9ac283d9db99cd634bad1951849
Subhradip Bose <subhradip.bose@Subhradips-MacBook-Air.local>
2024-01-25 16:24:50 +0530

    updated

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='ablv_classifier',
-    
     version='1.0.0',
-    #packages=find_packages(where="ablv_classifier",),
-    packages=find_packages(include=['ablv_classifier', 'ablv_classifier.*']),
-    #package_dir={"": "ablv_classifier"},
-    package_data={'': ['ablv_classifier/data/v1/*.csv']},
+    packages=find_packages(),
+    package_data={'ablv_classifier': ['*','data/v1/entity_list.csv', 'data/v1/type_mapping.csv']},
     include_package_data=True,
     install_requires=[
         'presidio_analyzer',
@@ -16,6 +13,5 @@ setup(
         "scikit-learn",
         "sentence_transformers",
         "numpy",
-
     ]
 )
