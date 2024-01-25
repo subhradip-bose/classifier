fc54ad210efa44eee9de982872bfaabba49c1df8
Subhradip Bose <subhradip.bose@Subhradips-MacBook-Air.local>
2024-01-25 16:00:42 +0530

    Updated

--- ablv_classifier/utils/file_util.py
+++ ablv_classifier/utils/file_util.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 class FileUtil:
     def read_type_mapping_file(self, version="v1"):
-        input_df = pd.read_csv("../data/v1/type_mapping.csv")
+        input_df = pd.read_csv("ablv_classifier/data/v1/type_mapping.csv")
         return input_df
     
     def read_entity_file(self, version="v1"):
-        input_df = pd.read_csv("../data/v1/entity_list.csv")
+        input_df = pd.read_csv("ablv_classifier/data/v1/entity_list.csv")
         return input_df
          
--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(
     name='ablv_classifier',
     
     version='1.0.0',
-    packages=find_packages(where="ablv_classifier"),
-    package_dir={"": "ablv_classifier"},
+    #packages=find_packages(where="ablv_classifier",),
+    packages=find_packages(include=['ablv_classifier', 'ablv_classifier.*']),
+    #package_dir={"": "ablv_classifier"},
+    package_data={'': ['ablv_classifier/data/v1/*.csv']},
     include_package_data=True,
     install_requires=[
         'presidio_analyzer',
