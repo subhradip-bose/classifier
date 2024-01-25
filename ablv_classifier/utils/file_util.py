d9098066b9ced6f4d70d33f1d1c645fb50f5eca8
Subhradip Bose <subhradip.bose@Subhradips-MacBook-Air.local>
2024-01-25 16:34:16 +0530

    Update file_util.py

--- ablv_classifier/utils/file_util.py
+++ ablv_classifier/utils/file_util.py
@@ -1,11 +1,18 @@
 import pandas as pd
+import os
+from pathlib import Path
 
 class FileUtil:
     def read_type_mapping_file(self, version="v1"):
-        input_df = pd.read_csv("ablv_classifier/data/v1/type_mapping.csv")
+        input_df = pd.read_csv(self.get_relative_path("../data/v1/type_mapping.csv"))
         return input_df
     
     def read_entity_file(self, version="v1"):
-        input_df = pd.read_csv("ablv_classifier/data/v1/entity_list.csv")
+        input_df = pd.read_csv(self.get_relative_path("../data/v1/entity_list.csv"))
         return input_df
+    
+    def get_relative_path(self, path_to_file: str):
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, path_to_file)
+        return Path(__file__).parent / filename
          
