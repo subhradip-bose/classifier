b57e2b0827be351860c7289783067d2a35ef067c
Subhradip Bose <subhradip.bose@Subhradips-MacBook-Air.local>
2024-01-26 13:02:04 +0530

    Update main.py

--- ablv_classifier/example/main.py
+++ ablv_classifier/example/main.py
@@ -2,7 +2,7 @@ from  ablv_classifier.services.classifier import TextClassifier
 
 
 clas = TextClassifier()
-d = clas.classfyText("Abluva has a contact number 9049800905")
+d = clas.classfyText("Abluva has a contact number 5089000105")
 print(d)
  
  
